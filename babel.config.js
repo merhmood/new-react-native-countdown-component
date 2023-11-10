18add64c34d65c12c02dfc5826f55c918783b46d
merhmood <mahmudsuberu9@gmail.com>
2023-11-10 12:22:36 +0100

    feat: configure babel for react, react native

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-react',
+      '@babel/preset-env',
+    ],
+  };
+};
